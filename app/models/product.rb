188ca89a77611a7757890a225fc695b783963a3b
Darko <zokiboy@gmail.com>
2008-07-15 23:06:07 +0200

    Added price to the view, validation for title lenght, partials for form new/update product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,13 @@
 class Product < ActiveRecord::Base
   validates_presence_of :title, :description, :image_url
   validates_numericality_of :price
-  validates_uniqueness_of :title
+  validates_uniqueness_of :title, :message => "must be unique"
   validates_format_of :image_url, 
                       :with    => %r{\.(gif|jpg|png)$}i,
                       :message => "must be a URL for a GIF, JPG, or PNG image"
 
+  validates_length_of :title, :minimum => 10, :message => "is too short" 
+  
   protected
   def validate
     errors.add(:price, "should be at least 0.01") if price.nil? ||  price < 0.01
