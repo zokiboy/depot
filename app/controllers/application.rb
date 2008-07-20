845c13dcd68886a86678d7034cd7aba3b2d3d333
Darko <zokiboy@gmail.com>
2008-07-20 20:55:17 +0200

    Cart created.

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'dbe5bbb18589b50f9fabbd4a2315385f'
+  protect_from_forgery  :secret => 'dbe5bbb18589b50f9fabbd4a2315385f'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,6 +4,52 @@ class StoreController < ApplicationController
       #@products = Product.find(:all, :order=>"title")
       
       @time = Time.current.strftime("%Y-%m-%d %H:%M:%S")
+      
+      @count = increment_count
+  
   end
+
+  def add_to_cart
+    begin
+      product = Product.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      logger.error("Attempt to access invalid product #{params[:id]}")
+      redirect_to_index("Invalid product" )
+    else
+      @cart = find_cart
+      @cart.add_product(product)
+      session[:counter] = 0
+    end
+  end
+  
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index("Your cart is currently empty" )
+  end
+  
+
+  private
+  
+  def redirect_to_index(msg)
+    flash[:notice] = msg
+    redirect_to :action => :index
+  end
+  
+  def find_cart
+      session[:cart] ||= Cart.new
+      
+      #this is the same as
+      #unless session[:cart]
+        #session[:cart] = Cart.new
+      #end
+      #session[:cart]
+    end
+
+  def increment_count
+    session[:counter] ||= 0
+    session[:counter] += 1
+  end
+
+
 
 end
