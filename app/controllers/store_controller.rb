c2183ece2cb5cba742f7f9ff57462f3e152af8cf
Darko <zokiboy@gmail.com>
2008-07-26 20:33:13 +0200

    Added ajaxified cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,37 +1,50 @@
 class StoreController < ApplicationController
   def index
       @products = Product.find_products_for_sale
-      #@products = Product.find(:all, :order=>"title")
+      @cart = find_cart
       
-      @time = Time.current.strftime("%Y-%m-%d %H:%M:%S")
-      
-      @count = increment_count
+      #@time = Time.current.strftime("%Y-%m-%d %H:%M:%S")
+      #@count = increment_count
   
   end
 
-  def add_to_cart
+
+ def add_to_cart
+   begin                     
+     product = Product.find(params[:id])  
+   rescue ActiveRecord::RecordNotFound
+     logger.error("Attempt to access invalid product #{params[:id]}")
+     redirect_to_index("Invalid product")
+   else
+     @cart = find_cart
+     @current_item = @cart.add_product(product)
+     redirect_to_index unless request.xhr?
+   end
+ end
+   
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index unless request.xhr?
+  end
+  
+  def remove_from_cart
     begin
       product = Product.find(params[:id])
     rescue ActiveRecord::RecordNotFound
       logger.error("Attempt to access invalid product #{params[:id]}")
-      redirect_to_index("Invalid product" )
+      redirect_to_index("Invalid product")
     else
       @cart = find_cart
-      @cart.add_product(product)
-      session[:counter] = 0
+      @current_item = @cart.remove_product(product)
+      redirect_to_index unless request.xhr?
     end
-  end
-  
-  def empty_cart
-    session[:cart] = nil
-    redirect_to_index("Your cart is currently empty" )
   end
   
 
   private
   
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg=nil)
+    flash[:notice] = msg if msg
     redirect_to :action => :index
   end
   
